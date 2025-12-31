87d6e7288dcd4fec17a08804c356be40c113986b
ratib54 <ratibsway@gmail.com>
2025-12-31 15:09:01 +0600

    add style

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import Chart from '../Chart/Chart';
 
 const Banner = () => {
  return (
-  <div className=' mt-44 lg:mt-12'>
+  <div className=' mt-24 lg:mt-12'>
 
    <h1 className=' text-[30px] lg:text-[120px] text-center leading-[1] uppercase font-BBH'>welcome <span className='text-[#ac2525]'>Admins</span></h1>
    <div className="chart">
