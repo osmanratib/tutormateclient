df9e8a60e254a2bd85ced479c967155d063dcd77
ratib54 <ratibsway@gmail.com>
2026-01-04 23:19:24 +0600

    make responsive tutor detail page

--- src/Pages/TutorsDetail/TutorsDetail.jsx
+++ src/Pages/TutorsDetail/TutorsDetail.jsx
@@ -4,27 +4,106 @@ import { IoArrowBackCircle } from "react-icons/io5";
 
 const TutorsDetail = () => {
  const personData = useLoaderData();
- const { name, university, dept, exp, college, phone } = personData;
+ const { name, university, dept, exp, college, phone, image } = personData;
+
  return (
-  <div>
-   <div>
-    <Link to={'/confirm'}>
-     <button className='flex items-center gap-1' ><IoArrowBackCircle /> back</button>
-    </Link>
-   </div>
-   <div className='flex gap-32 justify-center  items-center border-[2px] rounded-xl border-[#3db154] font-Alegreya uppercase p-10 shadow-2xl shadow-[#7ec180] mt-12 '>
-    <img className='w-[350px] h-[400px] rounded-lg' src={`https://tutormateadminserver.vercel.app/uploads/${personData.image}`} alt="" />
-    <div>
-     <h1>Hey , i am <span className='bg-[#fff] text-black px-5 rounded-lg'>{name}</span></h1>
-     <h1> i am a student of <span className='bg-[#fff] text-black px-5 rounded-lg'>{university}</span>. </h1>
-     <h1>currently i pursuing my undergraduate under <span className='bg-[#fff] text-black px-5 rounded-lg'>{dept}</span></h1>
-     <h1> my college was  <span className='bg-[#fff] text-black px-5 rounded-lg'>{college}</span></h1>
-     <h1>i have <span className='bg-[#fff] text-black px-5 rounded-lg'>{exp}</span>experience in this tuition field .  </h1>
-     <h1>you can contact me by call . here is my phone number  : <span className='bg-[#fff] text-black px-5 rounded-lg'>{phone}</span></h1>
-     <a href={`tel:${phone} `}>
-      <button className='bg-[#841515] w-full h-[50px] rounded font-extrabold  uppercase mt-5'>Call</button>
+  <div className="px-4 sm:px-8 lg:px-16 py-6">
+
+   {/* Back Button */}
+   <Link to="/confirm">
+    <button className="flex items-center gap-2 text-sm sm:text-base mb-4">
+     <IoArrowBackCircle size={22} />
+     Back
+    </button>
+   </Link>
+
+   {/* Main Card */}
+   <div className="
+        flex flex-col lg:flex-row
+        gap-6 lg:gap-20
+        justify-center items-center
+        border-2 border-[#3db154]
+        rounded-xl
+        font-Alegreya uppercase
+        p-4 sm:p-6 lg:p-10
+        shadow-2xl shadow-[#7ec180]
+        mt-6
+      ">
+
+    {/* Image */}
+    <img
+     className="
+            w-full
+            max-w-[280px] sm:max-w-[320px] lg:max-w-[350px]
+            h-auto
+            rounded-lg
+            object-cover
+          "
+     src={image}
+     alt={name}
+    />
+
+    {/* Text Content */}
+    <div className="w-full text-sm sm:text-base space-y-3">
+     <h1>
+      Hey, I am{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {name}
+      </span>
+     </h1>
+
+     <h1>
+      I am a student of{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {university}
+      </span>
+     </h1>
+
+     <h1>
+      Currently pursuing undergraduate in{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {dept}
+      </span>
+     </h1>
+
+     <h1>
+      My college was{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {college}
+      </span>
+     </h1>
+
+     <h1>
+      I have{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {exp}
+      </span>{" "}
+      experience in this tuition field
+     </h1>
+
+     <h1>
+      You can contact me at{" "}
+      <span className="bg-white text-black px-3 py-1 rounded-lg inline-block">
+       {phone}
+      </span>
+     </h1>
+
+     {/* Call Button */}
+     <a href={`tel:${phone}`}>
+      <button className="
+              bg-[#841515]
+              w-full
+              h-[48px]
+              rounded
+              font-extrabold
+              uppercase
+              mt-4
+            ">
+       Call
+      </button>
      </a>
     </div>
+
    </div>
   </div>
  );
