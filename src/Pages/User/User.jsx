77e8c118f251f2f72c53b43e2b298b8fad0b9d30
ratib54 <ratibsway@gmail.com>
2026-01-07 18:53:39 +0600

    make resmonsive user card

--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-const User = ({user , handleDelete , idx}) => { 
- const {name , email} = user ; 
+const User = ({ user, handleDelete, idx }) => {
+ const { name, email } = user;
+
  return (
-  <div>
-   <div className="box flex justify-between items-center bg-white px-1 lg:px-7 py-2 lg:py-5 rounded-2xl">
-    <span className="bg-white text-black">{idx + 1}</span>
-    <h1 className="bg-white text-black">{email}</h1>
-    <h1 className="bg-white text-black font-bold capitalize">{name}</h1>
-    <button onClick={() => handleDelete(user._id)} className="text-black font-BBH">
-     <h1 className="bg-red-700 px-1 lg:px-5 py-1 rounded-sm">delete</h1>
-    </button>
-   </div>
+  <div className="lg:w-full lg:space-y-0 space-y-6 lg:flex items-center  justify-center ">
+   <ul className='lg:flex justify-between items-center gap-4' > 
+    <li>{idx + 1}</li>
+    <li className='w-[300px]' >email: {email}</li>
+    <li className='w-[300px]'>name : {name}</li>
+   </ul>  
+
+   <button onClick={handleDelete} className='font-Alegreya px-5 py-1 rounded bg-red-700' >Delete</button>
+
   </div>
  );
 };
